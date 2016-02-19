e0a455c8171e9335762864df2ecf9fb7297a8814
Mohan Kumar <mohankumar@Mohans-MacBook-Pro.local>
2016-02-19 17:48:46 +0530

    step3 adding error handling middleware at the lost because middleware interperts all the request

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,28 @@
 /**
  * Created by mohankumar on 19/02/16.
+ *
  */
+
+/***
+ * modules.exports allows to export many functionality
+ */
+
+module.exports.index = index;
+module.exports.login = login;
+module.exports.loginProcess = loginProcess;
+module.exports.chat = chat;
+
+ function index(req,res){
+     res.send("index");
+ }
+function login(req,res){
+     res.send("login");
+ }
+function loginProcess(req,res){
+     res.redirect('/');
+ }
+function chat(req,res){
+     res.send("chat");
+ }
+
+
--- server.js
+++ server.js
@@ -4,12 +4,21 @@
 
  var express = require('express');
  var app = express();
+ var routes  = require('./routes/index');
+var errorHandler = require('./middleware/errorHandlers');
 
- app.get('*',function (req,res){
+// middleware 1) our routes are just the final middleware function req,res next
 
-    res.send('hello this is  response');
 
- });
+
+
+
+ app.get('/', routes.index);
+ app.get('/login', routes.login);
+ app.post('/login',routes.loginProcess);
+ app.get('/chat',routes.chat);
+
+app.use(errorHandler.notFound);
 
 app.listen('4000');
 console.log("app listing to port 4000");
